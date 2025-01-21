76f1223b52f13bdbefefa3b27498b6b22c63f96c
nasrinnajafibb <nasrinnajafibb@gmail.com>
2025-01-21 13:07:21 +0330

    Initial commit

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+<header className="site-header">
+  <div className="header-container">
+    <img 
+      src="/assets/images/dr-javadi.jpg"
+      alt="دکتر محمد علی جوادی"
+      className="header-image"
+    />
+  </div>
+</header>
